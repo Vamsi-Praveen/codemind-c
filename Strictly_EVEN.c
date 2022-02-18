91f4536472c3ef6b1529b4c9ba501e0e57746e9a
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-02-18 22:47:12 +0530

    #include<stdio.h> #include<stdlib.h> int main() {     int *arr,n,i,c=0;     scanf("%d",&n);     arr=(int *)malloc(n*sizeof(int));     for(i=0;i<n;i++)     {         scanf("%d" ,&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             if(i%2==0)             {                 c++;             }             else             {                 c=0;             }         }     }     if(c==0)     {         printf("False");     }     else     {         printf("True");     } }

---
+++ Strictly_EVEN.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int *arr,n,i,c=0;
+    scanf("%d",&n);
+    arr=(int *)malloc(n*sizeof(int));
+    for(i=0;i<n;i++)
+    {
+        scanf("%d" ,&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            if(i%2==0)
+            {
+                c++;
+            }
+            else
+            {
+                c=0;
+            }
+        }
+    }
+    if(c==0)
+    {
+        printf("False");
+    }
+    else
+    {
+        printf("True");
+    }
+}
