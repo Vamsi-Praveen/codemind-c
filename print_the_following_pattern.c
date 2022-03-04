56587ec2d8cb456477d357f2525dde9f4b9674ea
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-03-04 10:59:50 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=n;j>=1;j--)         {             printf("%d ",j);
            }
            printf("
    ");
        }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int r,i,j;
-    scanf("%d",&r);
-    for(i=1;i<=r;i++)
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
     {
-        for(j=1;j<=i;j++)
+        for(j=n;j>=1;j--)
         {
-            printf("%d",j);
+            printf("%d ",j);
+        
         }
         printf("
 ");
     }
-}    
+}
