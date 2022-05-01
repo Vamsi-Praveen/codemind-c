2d7b204f388ac446a25327e93144e515b3dbd70d
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-05-01 20:33:53 +0530

    #include<stdio.h> int main() {     int n,i,j,r,c,arr[200][200];     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&arr[i][j]);         }     }     int sum=0,sum1=0;     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             if(arr[i][j]%2==0)             {                 sum=sum+arr[i][j];             }             else             {                 sum1=sum1+arr[i][j];             }         }     }     printf("%d %d",sum,sum1); }

---
+++ Sum_of_even_and__odd_elements_in_a_matrix.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,r,c,arr[200][200];
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int sum=0,sum1=0;
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            if(arr[i][j]%2==0)
+            {
+                sum=sum+arr[i][j];
+            }
+            else
+            {
+                sum1=sum1+arr[i][j];
+            }
+        }
+    }
+    printf("%d %d",sum,sum1);
+}
